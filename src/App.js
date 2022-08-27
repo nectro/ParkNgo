56b3428293d1b27e85385adbbb52c244c71ebe05
Samaresh Samanta <ss9261@srmist.edu.in>
2022-08-27 22:28:32 +0530

    add: dashboardpanel for vehicle record

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderComponent from './component/header/header.component';
+import DashboardPanelComponent from './component/dashboardPanel/dashboardPanel.component';
 
 function App() {
   return (
     <div className="App">
         <HeaderComponent />
+        <DashboardPanelComponent />
     </div>
   );
 }
---
+++ src/component/dashboardPanel/dashboardPanel.component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './dashboardPanel.module.css'
+
+function DashboardPanelComponent() {
+    return (
+        <div className={classes.majorContainer}>
+        <div className={classes.VehicleCard}>
+
+        </div>
+            <div className={classes.VehicleCard}>
+
+            </div>
+        </div>
+    )
+}
+
+export default DashboardPanelComponent
