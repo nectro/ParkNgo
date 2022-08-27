aa69caaae7f5c84baa061ccd1b673dc2aea5b4f8
Samaresh Samanta <ss9261@srmist.edu.in>
2022-08-28 03:37:25 +0530

    add: some minute details

--- src/component/historyModal/historyModal.component.js
+++ src/component/historyModal/historyModal.component.js
@@ -14,39 +14,43 @@ function HistoryModalComponent({report,setShowHistory}) {
                     <p className={classes.closeIcon} onClick={()=>setShowHistory(false)}>X</p>
                 </div>
                 <div className={classes.forScroll}>
-                    <table className={classes.info}>
-                        <tbody>
-                            <tr>
-                                <th>Vehicle Type</th>
-                                <th>Vehicle No</th>
-                                <th>Driver Name</th>
-                                <th>Check In Time</th>
-                                <th>Check Out Time</th>
-                            </tr>
-                            {
-                                report &&
-                                report.map( (r,index)=>
-                                    <tr key={index}>
-                                        <td>
-                                            {r.vehicleType}
-                                        </td>
-                                        <td>
-                                            {r.vehicleNo}
-                                        </td>
-                                        <td>
-                                            {r.driverName}
-                                        </td>
-                                        <td>
-                                            {r.checkInTime}
-                                        </td>
-                                        <td>
-                                            {r.checkOutTime}
-                                        </td>
-                                    </tr>
-                                 )
-                            }
-                        </tbody>
-                    </table>
+                    {
+                        report.length > 0 ?
+                        <table className={classes.info}>
+                            <tbody>
+                                <tr>
+                                    <th>Vehicle Type</th>
+                                    <th>Vehicle No</th>
+                                    <th>Driver Name</th>
+                                    <th>Check In Time</th>
+                                    <th>Check Out Time</th>
+                                </tr>
+                                {
+                                    report &&
+                                    report.map( (r,index)=>
+                                        <tr key={index}>
+                                            <td>
+                                                {r.vehicleType}
+                                            </td>
+                                            <td>
+                                                {r.vehicleNo}
+                                            </td>
+                                            <td>
+                                                {r.driverName}
+                                            </td>
+                                            <td>
+                                                {r.checkInTime}
+                                            </td>
+                                            <td>
+                                                {r.checkOutTime}
+                                            </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table> :
+                        <p>No vehicle haven't checked out yet...</p>
+                    }
                 </div>
             </div>
         </div>
