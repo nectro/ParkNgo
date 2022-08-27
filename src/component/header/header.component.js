5e54c58f7dcfcdb25932cadb019a6669dd723b7f
Samaresh Samanta <ss9261@srmist.edu.in>
2022-08-28 04:22:14 +0530

    add: responsive menu

--- src/component/header/header.component.js
+++ src/component/header/header.component.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './header.module.css'
 
 import car from '../../assets/icons/car.svg'
@@ -6,8 +6,11 @@ import truck from '../../assets/icons/truck.svg'
 import bike from '../../assets/icons/bike.svg'
 import Logo from '../../assets/icons/logo.svg'
 import History from '../../assets/icons/history.svg'
+import Bar from '../../assets/icons/bar.svg'
 
 function HeaderComponent({showType,setShowType,totalVehicles,setShowHistory}) {
+
+    const [modal, setModal] = useState(false)
     // console.log(showType)
     return (
         <div className={classes.majorContainer}>
@@ -24,6 +27,25 @@ function HeaderComponent({showType,setShowType,totalVehicles,setShowHistory}) {
                 <img src={truck} className={showType === 'truck'?classes.selected:''}  onClick={()=>{setShowType('truck')}}/>
                 <img src={bike} className={showType === 'bike'?classes.selected:''}  onClick={()=>{setShowType('bike')}}/>
             </div>
+            <div className={classes.vehicleTypeM}>
+                <p>Total:</p>
+                <p style={{backgroundColor:'#ffcc02', padding:'2px 6px',borderRadius:'4px'}}>{totalVehicles}</p>
+                <img src={Bar} style={{height:20}} onClick={()=>setModal(true)}/>
+            </div>
+            {
+                modal &&
+                <div className={classes.menu}>
+                    <p className={classes.closeIcon} onClick={()=>setModal(false)}>X</p>
+                    <img src={History} onClick={()=>{setShowHistory(true);setModal(false);}}/>
+                    <p>
+                        Show vehicle type:
+                    </p>
+                    <h2 className={showType === 'all'?classes.selected:''} onClick={()=>{setShowType('all')}}>All</h2>
+                    <img src={car} className={showType === 'car'?classes.selected:''}  onClick={()=>{setShowType('car');setModal(false);}}/>
+                    <img src={truck} className={showType === 'truck'?classes.selected:''}  onClick={()=>{setShowType('truck');setModal(false);}}/>
+                    <img src={bike} className={showType === 'bike'?classes.selected:''}  onClick={()=>{setShowType('bike');setModal(false);}}/>
+                </div>    
+            }
         </div>
     )
 }
